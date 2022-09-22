edf51803f9672ed82f753560b66e6a83bd1fb85e
MahiSoftDev <113410327+MahiSoftDev@users.noreply.github.com>
2022-09-23 02:08:08 +0530

    MethodOverLoading'

--- 9 Sep 2022 invoke method of other class/Division.java
+++
@@ -1,7 +0,0 @@
-class Division{
-	public static void divide(double a,double b){
-		double div=Math.round(a/b*100);
-		double div0=div/100;
-		System.out.println("\n"+a+" / " + b  +" = "+div0+"\n");
-	}
-}
